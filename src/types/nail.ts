c3bd0c3013cdcebe3bec94e2b776d5e6c53e2ba1
LeeWxx <dnckd0903@naver.com>
2025-02-25 15:28:11 +0900

    feat: 쉐잎, 카테고리, 컬러 타입 추가

---
+++ src/types/nail.ts
@@ -0,0 +1,7 @@
+export type Shape = "SQUARE" | "ROUND" | "ALMOND" | "BALLERINA" | "STILETTO"
+export type Category = "ONE COLOR" | "FRENCH" | "GRADIENT" | "ART"
+export type Color = "WHITE" | "BLACK" | "BEIGE" | "PINK" | "YELLOW" | "GREEN" | "BLUE" | "SILVER"
+
+export const SHAPES: Shape[] = ["SQUARE", "ROUND", "ALMOND", "BALLERINA", "STILETTO"]
+export const CATEGORIES: Category[] = ["ONE COLOR", "FRENCH", "GRADIENT", "ART"]
+export const COLORS: Color[] = ["WHITE", "BLACK", "BEIGE", "PINK", "YELLOW", "GREEN", "BLUE", "SILVER"] 
