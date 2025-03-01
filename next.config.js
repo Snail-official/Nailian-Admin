2dc0d91ed6186544514739221ff6a3f206e741ac
LeeWxx <dnckd0903@naver.com>
2025-03-01 22:33:21 +0900

    feat: Next.js 설정 업데이트:  이미지 도메인 및 assetPrefix 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const s3Domain = process.env.AWS_S3_URL
+  ? process.env.AWS_S3_URL.replace(/^https?:\/\//, '')
+  : '';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     turbo: {
       rules: {
@@ -9,4 +14,17 @@ module.exports = {
       },
     },
   },
-}
+  images: {
+    domains: [s3Domain, 'cdn.snail.enak.kr'].filter(Boolean),
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? `${process.env.AWS_CLOUDFRONT_DOMAIN}` : '',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
